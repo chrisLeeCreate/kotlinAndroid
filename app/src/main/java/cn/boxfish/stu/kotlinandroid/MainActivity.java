978e699c6ef1c4b8b4da43fcc26a71b373d69830
lishaowei <lishaowei@boxfish.cn>
2017-05-15 16:04:47 +0800

    fujiuhong

--- app/src/main/java/cn/boxfish/stu/kotlinandroid/MainActivity.java
+++ app/src/main/java/cn/boxfish/stu/kotlinandroid/MainActivity.java
@@ -10,6 +10,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        fujiuhong();
+    }
+
+    private void fujiuhong() {
     }
 
 }
