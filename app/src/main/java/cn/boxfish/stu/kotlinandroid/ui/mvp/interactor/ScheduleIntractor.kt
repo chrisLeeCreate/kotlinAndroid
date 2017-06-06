f325fa2da49fc278417970a44e1976bc1da0968c
lishaowei <lishaowei@boxfish.cn>
2017-06-06 17:55:14 +0800

    kotlin+mvp插件

---
+++ app/src/main/java/cn/boxfish/stu/kotlinandroid/ui/mvp/interactor/ScheduleIntractor.kt
@@ -0,0 +1,13 @@
+package cn.boxfish.stu.kotlinandroid.schedule;
+
+import android.content.Context;
+
+/**
+ * @author lishaowei
+ * @Package cn.boxfish.stu.kotlinandroid.schedule
+ * @Description: $description
+ * @date 2017/06/06
+ */
+public class ScheduleIntractor : ScheduleContract.Intractor{
+
+}
---
+++ app/src/main/java/cn/boxfish/stu/kotlinandroid/ui/mvp/view/ScheduleContract.kt
@@ -0,0 +1,17 @@
+package cn.boxfish.stu.kotlinandroid.schedule;
+
+
+/**
+ * @author lishaowei
+ * @Package cn.boxfish.stu.kotlinandroid.schedule
+ * @Description: $description
+ * @date 2017/06/06
+ */
+public interface ScheduleContract {
+
+    interface View {}
+
+    interface Presenter {}
+
+    interface Intractor {}
+}
