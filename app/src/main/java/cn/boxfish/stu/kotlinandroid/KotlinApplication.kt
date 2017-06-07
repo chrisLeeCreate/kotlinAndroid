338323b4888c22209aef7fed9b29ac4c3dcfea63
lishaowei <lishaowei@boxfish.cn>
2017-06-07 11:20:21 +0800

    kotlin+多渠道打包

--- app/src/main/java/cn/boxfish/stu/kotlinandroid/KotlinApplication.kt
+++ app/src/main/java/cn/boxfish/stu/kotlinandroid/KotlinApplication.kt
@@ -1,12 +1,20 @@
 package cn.boxfish.stu.kotlinandroid
 
 import android.app.Application
+import android.content.Context
+import com.mcxiaoke.packer.helper.PackerNg
 
 /**
  * Created by lishaowei on 2017/5/19.
  */
 class KotlinApplication : Application() {
+    lateinit var instance: KotlinApplication
     override fun onCreate() {
         super.onCreate()
+        val market = PackerNg.getMarket(context(), "lishaowei")
+    }
+
+    fun context(): Context {
+        return instance.applicationContext
     }
 }
