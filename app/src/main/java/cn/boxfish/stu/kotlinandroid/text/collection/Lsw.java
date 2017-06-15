b3ffbbcbf40dcb36e0f0a9c09f9cf35c6fa93a10
lishaowei <lishaowei@boxfish.cn>
2017-06-15 17:35:11 +0800

    kotlin+            versionNameSuffix " - " + System.getProperty("user.name")

---
+++ app/src/main/java/cn/boxfish/stu/kotlinandroid/text/collection/Lsw.java
@@ -0,0 +1,28 @@
+package cn.boxfish.stu.kotlinandroid.text.collection;
+
+/**
+ * Created by lishaowei on 2017/6/15.
+ */
+
+public class Lsw extends Lishaowei {
+    public static void main(String[] args) throws ClassNotFoundException {
+//        //1
+//        Lsw lsw = new Lsw();
+//        System.out.println(lsw.getClass().getName());
+//        System.out.println(lsw.getClass().toString());
+
+        //2
+//        Class<?> class1 = null;
+//        Class<?> class2 = null;
+//        Class<?> class3 = null;
+//        class1 = Class.forName("cn.boxfish.stu.kotlinandroid.text.collection.Lsw");
+//        class2 = new Lsw().getClass();
+//        class3 = Lsw.class;
+//        System.out.println("类名字是:" + class1);
+//        System.out.println("类名字是:" + class2);
+//        System.out.println("类名字是:" + class3);
+
+        //3
+
+    }
+}
